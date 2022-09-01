c1640913a9dafece13812ee2a103478e62a31da2
t-creates <geislinger@pm.me>
2022-09-01 16:40:00 -0600

    Project Structure and components. Navbar is started

--- tg-portfolio/components/Navbar.jsx
+++ tg-portfolio/components/Navbar.jsx
@@ -56,9 +56,10 @@ const Navbar = () => {
       <div className="hidden mobile-menu">
         <ul className="">
           <li className="active"><a href="index.html" className="block text-sm px-2 py-4 text-white bg-green-500 font-semibold">Home</a></li>
-          <li><a href="#services" className="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300">Services</a></li>
-          <li><a href="#about" className="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300">About</a></li>
-          <li><a href="#contact" className="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300">Contact Us</a></li>
+          <li><Link href="/services" className="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300">Services</Link></li>
+          <li><Link href="/about" className="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300">About</Link></li>
+          <li><Link href="/portfolio" className="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300">Contact Us</Link></li>
+          <li><Link href="/contact" className="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300">Contact Us</Link></li>
         </ul>
       </div>
 
