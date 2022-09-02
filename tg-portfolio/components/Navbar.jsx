a5df57e627da76ea692ed7f08660eea9ab862e5d
Travis Geislinger <geislinger@pm.me>
2022-09-02 09:30:35 -0600

    Update Navbar.jsx

--- tg-portfolio/components/Navbar.jsx
+++ tg-portfolio/components/Navbar.jsx
@@ -5,10 +5,32 @@ const Navbar = () => {
   console.log('Navbar');
   // const btn = document.querySelector('button.mobile-menu-button');
   // const menu = document.querySelector('.mobile-menu');
+  const navLinks = [
+    {
+      id: 'nav-link-1',
+      name: 'About',
+      link: '/about',
+    },
+    {
+      id: 'nav-link-2',
+      name: 'Services',
+      link: '/services',
+    },
+    {
+      id: 'nav-link-3',
+      name: 'Portfolio',
+      link: '/portfolio',
+    },
+    {
+      id: 'nav-link-4',
+      name: 'Contact',
+      link: '/contact',
+    },
+  ];
   return (
     // Navbar
     <nav className=" bg-cyan-400/75 shadow-lg">
-      <div className="max-w-10xl mx-auto px-48 md:px-16">
+      <div className="max-w-10xl mx-auto lg:px-48 md:px-16">
         <div className="flex justify-between">
           <div className="flex lg:space-x-7">
             <div />
@@ -21,30 +43,14 @@ const Navbar = () => {
             </Link>
           </div>
           <div className="hidden md:flex items-center space-x-11">
-            <Link href="/">
-              <p className="cursor-pointer py-4 px-2 border-b-4 border-zinc-500 font-semibold
+            {navLinks.map((navItem) => (
+              <Link href={navItem.link} key={navItem.id}>
+                <p className="cursor-pointer py-4 px-2 border-b-4 border-zinc-500 font-semibold
               hover:animate-pulse hover:text-zinc-800 navName"
-              >About
-              </p>
-            </Link>
-            <Link href="/">
-              <p className="cursor-pointer py-4 px-2 border-b-4 border-zinc-500 font-semibold
-              hover:animate-pulse hover:text-zinc-800 navName"
-              >Services
-              </p>
-            </Link>
-            <Link href="/">
-              <p className="cursor-pointer py-4 px-2 border-b-4 border-zinc-500 font-semibold
-              hover:animate-pulse hover:text-zinc-800 navName"
-              >Porfolio
-              </p>
-            </Link>
-            <Link href="/">
-              <p className="cursor-pointer py-4 px-2 border-b-4 border-zinc-500 font-semibold
-              hover:animate-pulse hover:text-zinc-800 navName"
-              >Contact
-              </p>
-            </Link>
+                >{navItem.name}
+                </p>
+              </Link>
+            ))}
           </div>
         </div>
         {/* Mobile Menu Button */}
@@ -55,11 +61,11 @@ const Navbar = () => {
       {/* Mobile Menu */}
       <div className="hidden mobile-menu">
         <ul className="">
-          <li className="active"><a href="index.html" className="block text-sm px-2 py-4 text-white bg-green-500 font-semibold">Home</a></li>
-          <li><Link href="/services" className="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300">Services</Link></li>
-          <li><Link href="/about" className="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300">About</Link></li>
-          <li><Link href="/portfolio" className="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300">Contact Us</Link></li>
-          <li><Link href="/contact" className="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300">Contact Us</Link></li>
+          {navLinks.map((navItem) => (
+            <Link href={navItem.link} key={navItem.key}>
+              <li className="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300">{navItem.name}</li>
+            </Link>
+          ))}
         </ul>
       </div>
 
