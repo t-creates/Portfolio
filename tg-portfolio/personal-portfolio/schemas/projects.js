550efe76ffb18342a77c215b9e3e41ed48e0ce27
t-creates <geislinger@pm.me>
2023-02-10 20:40:24 -0700

    backend schema update & homepage title css

--- tg-portfolio/personal-portfolio/schemas/projects.js
+++ tg-portfolio/personal-portfolio/schemas/projects.js
@@ -9,51 +9,50 @@ export default {
       type: 'string',
     },
 
-      {
-        name: 'description',
-        title: 'Description',
-        description: 'One or two sentences about yourself, or a favourite quote/tagline.',
-        type: 'text',
-      },
-      {
-        name: 'image',
-        title: 'Image',
-        type: 'image',
-        options: {
-          hotspot: true
-        }
-      },
-      {
-        name: 'neww',
-        title: 'Neww',
-        type: 'boolean',
-      },
-      {
-        name: 'sourceCode',
-        title: 'Source Code',
-        type: 'url',
-      },
-      {
-        name: 'website',
-        title: 'Website',
-        type: 'url',
-      },
-      {
-        name: 'badge',
-        title: 'Badge',
-        type: 'array',
-        of: [{type: 'string'}]
-      },
     {
-     // Slug -> like a url, a unique string
-     name: 'slug',
-     title: 'Slug',
-     type: 'slug',
-     options: {
-       source: 'name',
-       maxLength: 90,
-     }
+      name: 'description',
+      title: 'Description',
+      description: 'One or two sentences about the project.',
+      type: 'text',
     },
-
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true
+      }
+    },
+    {
+      name: 'neww',
+      title: 'Neww',
+      type: 'boolean',
+    },
+    {
+      name: 'sourceCode',
+      title: 'Source Code',
+      type: 'url',
+    },
+    {
+      name: 'website',
+      title: 'Website',
+      type: 'url',
+    },
+    {
+      name: 'badge',
+      title: 'Badge',
+      type: 'array',
+      of: [{ type: 'string' }]
+    },
+    {
+      // Slug -> like a url, a unique string
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxLength: 90,
+      }
+    },
   ]
 };
