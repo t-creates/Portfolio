083b527aa311a204a07b1b08aaa172ef752c0ec3
t-creates <geislinger@pm.me>
2024-04-27 17:26:26 -0600

    Updates

---
+++ tg-portfolio/components/about/AppVsSite.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const AppVsSite = () => (
+  <div className="sm:p-2 md:p-6 animate-in slide-in-from-bottom-48 duration-1000">
+    <h1 className="aboutTitle text-black sm:text-4xl md:text-5xl font-bold pb-5">Do you Want An Web Application Or Website?</h1>
+    <div className="text-xl">
+      <p className="pt-5 text-black/75 font-medium test">Web applications are programs or software made by third parties,
+        hosted on a remote server, and accessible from any web browser on any device.
+      </p>
+      <p className="pt-5 text-black/75 font-medium test">A website, on the other hand, is mostly about giving information. </p>
+    </div>
+  </div>
+);
+
+export default AppVsSite;
